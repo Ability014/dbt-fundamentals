509e6e5fe690cb3daf9219d3c9e2c1172c60becb
abbisco.ae@gmail.com <abbisco.ae@gmail.com>
2024-06-01 11:34:09 +0000

    made changes to the macro: generate_schema_name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,7 +2,7 @@
 
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none or target.name != 'RAW' -%}
+    {%- if custom_schema_name is none or target.name != 'dev' -%}
 
         {{ default_schema }}
 
